9f53e78925705c3b15188cad7f20a7bb387c8fde
Nirmal <enirmal87@gmail.com>
2026-03-31 00:19:47 +0530

    seventh commit

---
+++ Area.java
@@ -0,0 +1,30 @@
+class Shapes
+{
+int area(int a)
+{
+return a*a;
+}
+int area(int a,int b)
+{
+return a*b;
+
+
+
+}
+double area(double r)
+{
+return 3.14*r*r;
+}
+}
+
+public class Area
+{
+public static void main(String args[])
+{
+Shapes sh= new Shapes();
+System.out.println("Area Of Sqaure of length 5: "+sh.area(5));
+System.out.println("Area Of Rectangle of length 5 and breadth 4:
+"+sh.area(5,4));
+System.out.println("Area Of Circle of radius 2: "+sh.area(2.00));
+}
+}
